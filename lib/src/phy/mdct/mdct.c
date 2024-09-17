8eae91d329cc16acd5074bb5a542c272b427ed1b
Evan Chen <daevanchen@gmail.com>
2024-09-17 19:00:55 +0800

    refactor: removed unwanted code

--- lib/src/phy/mdct/mdct.c
+++ lib/src/phy/mdct/mdct.c
@@ -11,45 +11,6 @@
 // XXX copied from pss_nr.c, should be in a common header
 #define PSS_NR_SUBC_BEGIN 56
 
-/*
-static cf_t time_domain_pss[SRSRAN_NOF_NID_2_NR][SRSRAN_MDCT_PSS_FFT_SIZE];
-
-// Prepare all time domain PSS sequences for all NID2's
-static void prepare_all_nr_pss_x() {
-  static bool initialized = false;
-  if (initialized) {
-    return;
-  }
-
-  uint32_t N_id_2;
-  cf_t ssb_grid[SRSRAN_SSB_NOF_RE];
-//  cf_t pss_nr[SRSRAN_PSS_NR_LEN];
-  uint32_t pss_loc_in_ssb = SRSRAN_PSS_NR_SYMBOL_IDX * SRSRAN_SSB_BW_SUBC + PSS_NR_SUBC_BEGIN;
-  srsran_dft_plan_t ifft_plan;
-  cf_t temp[SRSRAN_MDCT_PSS_FFT_SIZE];
-//  int factor = floor(SRSRAN_MDCT_PSS_FFT_SIZE / SRSRAN_PSS_NR_LEN);
-  int center_shift = floor((float)SRSRAN_PSS_NR_LEN / 2.0);
-
-  // TODO: Could be optimized by multiplying the constant phase instead of computing the IFFT for each N_id_2
-  for (N_id_2 = 0; N_id_2 < SRSRAN_NOF_NID_2_NR; N_id_2++) {
-    srsran_pss_nr_put(ssb_grid, N_id_2, 1.0f);
-    memset(temp, 0, SRSRAN_MDCT_PSS_FFT_SIZE * sizeof(cf_t));
-    memcpy(temp + center_shift, &ssb_grid[pss_loc_in_ssb], SRSRAN_PSS_NR_LEN * sizeof(cf_t));
-    srsran_dft_plan_c(&ifft_plan, SRSRAN_MDCT_PSS_FFT_SIZE, SRSRAN_DFT_BACKWARD);
-    srsran_dft_run_c(&ifft_plan, temp, time_domain_pss[N_id_2]);
-    srsran_dft_plan_free(&ifft_plan);
-  }
-  initialized = true;
-}
-
-// XXX for testing
-void get_time_domain_pss(cf_t* out, uint32_t N_id_2)
-{
-  prepare_all_nr_pss_x();
-  memcpy(out, time_domain_pss[N_id_2], SRSRAN_PSS_NR_LEN * sizeof(cf_t));
-}
-*/
-
 static cf_t calculate_D(const cf_t* y, const cf_t* x_tilde, cf_t* temp, cf_t* output, uint32_t n, uint32_t Q, uint32_t psi)
 {
   int32_t d = (int32_t)(1 + (Q * psi));
@@ -83,6 +44,7 @@ static void prepare_pss_x(srsran_pss_mdct_t* mdct)
   int N_id_2;
   srsran_dft_plan_t ifft_plan;
 //  srsran_dft_plan_c(&ifft_plan, mdct->n, SRSRAN_DFT_BACKWARD);
+  // TODO: Could be optimized by multiplying the constant phase instead of computing the IFFT for each N_id_2
   for (N_id_2 = 0; N_id_2 < SRSRAN_NOF_NID_2_NR; N_id_2++) {
     mdct->pss_x[N_id_2] = (cf_t*)malloc(mdct->n * sizeof(cf_t));
     srsran_dft_plan_guru_c(&ifft_plan, (int)mdct->n, SRSRAN_DFT_BACKWARD, mdct->temp, mdct->pss_x[N_id_2], 1, 1, 1, 1, 1);
@@ -164,7 +126,7 @@ SRSRAN_API int mdct_detect_pss(const srsran_pss_mdct_t* mdct,
   float peak = -1 * INFINITY;
   // TODO: Should be optimized by multiplying the constant phase instead of computing MDCT for each N_id_2
   for (uint32_t N_id_2 = 0; N_id_2 < SRSRAN_NOF_NID_2_NR; N_id_2++) {
-    for (uint32_t tau = 0; tau < nof_samples - mdct->n; tau += window_sz) {
+    for (int32_t tau = 0; tau < nof_samples - mdct->n; tau += (int)window_sz) {
       cf_t corr = calculate_C(mdct, in + tau, N_id_2, tau);
       float corr_mag = cabsf(corr);
       if (corr_mag > peak) {
