4a7421977ad4263b934c0739c1c7b017267b4c68
Evan Chen <daevanchen@gmail.com>
2024-09-20 18:09:26 +0800

    refactor: adopt srsRAN built-in cyclic inner product function on differential product

--- lib/src/phy/mdct/differential_prod.c
+++ lib/src/phy/mdct/differential_prod.c
@@ -6,32 +6,11 @@
 #include "srsran/phy/utils/vector.h"
 #include <stdlib.h>
 
-void srsran_vec_prod_conj_cyclic_ccc(const cf_t* a, const cf_t* b, cf_t* c, int32_t shifts, uint32_t n)
-{
-  uint32_t offset = (uint32_t)abs(shifts);
+// TODO move this to vector.h
+void ssb_vec_prod_conj_circ_shift(const cf_t* a, const cf_t* b, cf_t* c, uint32_t n, int shift);
 
-  // Avoid negative of samples
-  if (offset > n) {
-    srsran_vec_cf_zero(c, n);
-    return;
-  }
-
-  if (shifts > 0) {
-    srsran_vec_prod_conj_ccc(&a[0], &b[offset], &c[0], n - offset);
-    srsran_vec_prod_conj_ccc(&a[n - offset], &b[0], &c[n - offset], offset);
-    return;
-  }
-
-  if (shifts < 0) {
-    srsran_vec_prod_conj_ccc(&a[offset], &b[0], &c[0], n - offset);
-    srsran_vec_prod_conj_ccc(&a[0], &b[n - offset], &c[n - offset], offset);
-    return;
-  }
-
-  srsran_vec_prod_conj_ccc(a, b, c, n);
-}
-
+// TODO make this inline?
 void differential_product(const cf_t* a, cf_t* c, int32_t d, uint32_t n)
 {
-  srsran_vec_prod_conj_cyclic_ccc(a, a, c, d, n);
+  ssb_vec_prod_conj_circ_shift(a, a, c, n, d);
 }
